4fab0ccc2816c9453a3844504d2109f375bb4fa3
Maciej Olejnik <maciej.olejnik.pannoire@gmail.com>
2020-08-18 20:47:36 +0200

    Init project structure

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  bracketSpacing: true,
+  jsxBracketSameLine: true,
+  arrowParens: 'avoid',
+  singleQuote: true,
+  trailingComma: 'all',
+  useTabs: false,
+  semi: true,
+};
---
+++ README.md
@@ -0,0 +1,7 @@
+# :space_invader: React Native Typescript
+
+## You can change project name like this
+
+```
+react-native-rename "App Name" -b com.company.name
+```
