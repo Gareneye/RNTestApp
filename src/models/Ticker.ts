31b807c6dcad73f5b82769740c9249b2e074118c
Maciej Olejnik <maciej.olejnik.pannoire@gmail.com>
2020-08-20 12:34:55 +0200

    Add Quote model

--- src/models/Ticker.ts
+++ src/models/Ticker.ts
@@ -1,16 +1,17 @@
+import { Quote } from './Quote';
+import { Type } from 'class-transformer';
 
 export class Ticker {
-    public readonly id!: string;
-    public readonly name!: string;
-    public readonly symbol!: string;
-    public readonly rank!: number;
-    public readonly circulating_supply!: number;
-    public readonly total_supply!: number;
-    public readonly max_supply!: number;
-    public readonly beta_value!: number;
-    public readonly last_updated!: string;
+  public readonly id!: string;
+  public readonly name!: string;
+  public readonly symbol!: string;
+  public readonly rank!: number;
+  public readonly circulating_supply!: number;
+  public readonly total_supply!: number;
+  public readonly max_supply!: number;
+  public readonly beta_value!: number;
+  public readonly last_updated!: string;
 
-    // Todo: Create model for Quote
-    public readonly quotes!: {[index: string]: any};
-  }
- 
+  @Type(() => Quote)
+  public readonly quotes!: {[index: string]: Quote}; // Info: Map<string, Quote> doesn't work
+}
--- src/modules/home/atoms/CoinItem.tsx
+++ src/modules/home/atoms/CoinItem.tsx
@@ -3,6 +3,7 @@ import { StyleSheet, Text, Image, View } from 'react-native';
 import { Ticker } from 'models/Ticker';
 import { Fonts, Dimens, Colors } from 'resources';
 import { SvgUri } from 'react-native-svg';
+import { Quote } from 'models/Quote';
 
 interface Props {
   item: Ticker;
@@ -15,12 +16,16 @@ export const CoinItem = (props: Props) => {
   const imageUrl = `https://static2.coinpaprika.com/coin/${ticker.id}/logo-thumb.png`;
   const chartUrl = `https://graphs2.coinpaprika.com/currency/chart/${ticker.id}/7d/chart.svg`;
 
+  const tempQuote: Quote | undefined = ticker.quotes['USD'];
+  const value = tempQuote.price.toFixed(2);
+  const percentChange = tempQuote.percent_change_7d;
+
   return (
     <View style={styles.row}>
       <View style={styles.col}>
         <View style={styles.headerRow}>
           <Image source={{ uri: imageUrl }} style={styles.icon} />
-          <Text style={styles.title}> {ticker.symbol}</Text>
+          <Text style={styles.title}>{ticker.symbol}</Text>
         </View>
         <View style={[styles.headerRow, styles.paddedRow]}>
           <Text style={styles.rank}>{ticker.rank}</Text>
@@ -32,9 +37,11 @@ export const CoinItem = (props: Props) => {
         <SvgUri width={100} height={'100%'} uri={chartUrl} />
       </View>
 
-      <View style={styles.col}>
-        <Image source={{ uri: imageUrl }} style={{ width: 20, height: 20 }} />
-        <Text>{ticker.name}</Text>
+      <View style={[styles.col, styles.lastCol]}>
+        <Text style={styles.title}>{value}</Text>
+        <View style={[styles.changeLabel, percentChange > 0 ? styles.changeLabelUp : styles.changeLabelDown]}>
+          <Text style={styles.changeLabelText}>{`${percentChange}%`}</Text>
+        </View>
       </View>
     </View>
   );
@@ -49,6 +56,9 @@ const styles = StyleSheet.create({
   },
   col: {
     flexDirection: 'column',
+  },
+  lastCol: {
+    alignItems: 'flex-end'
   },
   headerRow: {
     flexDirection: 'row',
@@ -65,6 +75,7 @@ const styles = StyleSheet.create({
     ...Fonts.label15,
   },
   rank: {
+    ...Fonts.caption13,
     backgroundColor: Colors.border,
     lineHeight: 20,
     minWidth: 20,
@@ -78,4 +89,22 @@ const styles = StyleSheet.create({
     ...Fonts.body15,
     color: Colors.desc,
   },
+  changeLabel: {
+    minWidth: 20,
+    height: 28,
+    alignItems: 'flex-end',
+    paddingHorizontal: Dimens.space.s,
+    borderRadius: 5
+  },
+  changeLabelText: {
+    ...Fonts.label15,
+    lineHeight: 28,
+    color: 'white',
+  },
+  changeLabelUp: {
+    backgroundColor: 'green',
+  },
+  changeLabelDown: {
+    backgroundColor: 'red',
+  }
 });
